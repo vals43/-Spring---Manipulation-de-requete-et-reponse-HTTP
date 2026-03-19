c9f165266cf5f6dc6c3089290917c268d6cf9def
Vals43 <hei.teddy.3@gmail.com>
2026-03-19 22:35:37 +0300

    Initial commit

---
+++ src/main/java/prog3/td2_3/Td23Application.java
@@ -0,0 +1,13 @@
+package prog3.td2_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td23Application.class, args);
+    }
+
+}
---
+++ src/test/java/prog3/td2_3/Td23ApplicationTests.java
@@ -0,0 +1,13 @@
+package prog3.td2_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
